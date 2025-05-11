3c0f686afb9435fb78189baef7b65911558cd32f
최유정 <yyyy2549@gmail.com>
2025-05-11 23:57:09 +0900

    엔티티 클래스 개발 0.1

---
+++ spring-jpa1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Entity
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+    private int stockQuantity;
+
+}
